3fb26919618daf416820ce28884ef5f1a09e7424
Abhishek Singh <prince9singh@gmail.com>
2020-05-25 15:42:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var pip= document.getElementsByClassName("vjs-picture-in-picture-control")[0].remove();
+var pip= document.getElementsByClassName("vjs-picture-in-picture-control");
 if(pip[0] && pip[0].offsetParent){
   pip[0].remove();
 }
