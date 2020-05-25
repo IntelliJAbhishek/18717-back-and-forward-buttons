5d5f900a7766c7b2fbbe45b021405e109fa82835
Abhishek Singh <prince9singh@gmail.com>
2020-05-25 15:47:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-var pip= document.getElementsByClassName("vjs-picture-in-picture-control");
-if(pip[0] && pip[0].offsetParent){
-  pip[0].remove();
-}
+const player=videojs.getPlayer(document.getElementsByTagName('video-js')[0].id);
+player.ready(()=>{
+  var pip= document.getElementsByClassName("vjs-picture-in-picture-control");
+  if(pip[0] && pip[0].offsetParent){
+    pip[0].remove();
+  }
+});
+
