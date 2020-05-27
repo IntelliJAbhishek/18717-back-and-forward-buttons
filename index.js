c36b33a8dd2a0739c7109abff3633a7979d53bb9
Abhishek Singh <prince9singh@gmail.com>
2020-05-27 16:19:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const player=videojs.getPlayer(document.getElementsByTagName('video-js')[0].id);
-player.ready(()=>{
-  var pip= document.getElementsByClassName("vjs-picture-in-picture-control");
-  if(pip[0] && pip[0].offsetParent){
-    pip[0].remove();
-  }
-});
+videojs.registerPlugin('errorHandler', function() {
+  var myPlayer = this;
+  myPlayer.on('error',()=>{
+        window.ReactNativeWebView.postMessage(JSON.stringify({type:'error'})) 
+  });
+})
 
